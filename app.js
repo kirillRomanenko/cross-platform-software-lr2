25d74ee0ea3f956771d7f08f481164e2a2012dd4
Кирилл Романенко <romanenko.kirill1995@gmail.com>
2020-04-01 15:46:08 +0300

    fix small

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const readlineSync = require('readline-sync');
-const { ConvertTemperature, ConvertLength } = require('./convert');
+const { ConvertTemp, ConvertLen } = require('./convert');
 
 let select = readlineSync.question('Выберите вариант: \n 1-перевод температуры \n 2-перевод длины\n');
 if (Number(select) == 1) {
     let temperatureC = readlineSync.question('Введите температуру по Цельсию: ');
-    let temperature = new ConvertTemperature(temperatureC);
+    let temperature = new ConvertTemp(temperatureC);
     temperature.convert();
 } else if (Number(select) == 2) {
     let centimeters = readlineSync.question('Введите длину в сантиметрах: ');
-    let Length = new ConvertLength(centimeters);
+    let Length = new ConvertLen(centimeters);
     Length.convert();
 } else {
     console.log('Выбран неверный вариант!');
--- convert.js
+++ convert.js
@@ -1,17 +1,17 @@
-class ConvertTemperature {
+class ConvertTemp {
     constructor(value) {
         this.value = Number(value);
     }
     convert() {
         let kelvin = this.value + 273.15;
         let fahrenheit = (this.value * (9 / 5)) + 32;
-        console.log('Температура по Цельсию: ', this.value);
-        console.log('Температура в Кельвинах: ', kelvin);
-        console.log('Температура по Фаренгейту: ', fahrenheit);
+        console.log('Температура (по Цельсию): ', this.value);
+        console.log('Температура (в Кельвинах): ', kelvin);
+        console.log('Температура (по Фаренгейту): ', fahrenheit);
     }
 }
 
-class ConvertLength {
+class ConvertLen {
     constructor(value) {
         this.value = Number(value);
     }
@@ -21,12 +21,12 @@ class ConvertLength {
         let feet = this.value / 30.48;
         let inches = this.value / 2.54;
         let yards = this.value / 91.44;
-        console.log('Длина в сантиметрах: ', this.value);
-        console.log('Длина в метрах: ', meters);
-        console.log('Длина в километрах: ', kilometers);
-        console.log('Длина в футах: ', feet);
-        console.log('Длина в дюймах: ', inches);
-        console.log('Длина в ярдах: ', yards);
+        console.log('Длина (в сантиметрах): ', this.value);
+        console.log('Длина (в метрах): ', meters);
+        console.log('Длина (в километрах): ', kilometers);
+        console.log('Длина (в футах): ', feet);
+        console.log('Длина (в дюймах): ', inches);
+        console.log('Длина (в ярдах): ', yards);
     }
 }
-module.exports = { ConvertTemperature, ConvertLength }
+module.exports = { ConvertTemp, ConvertLen }
