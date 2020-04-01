ad8e07aa4ce6afb61d31f00e8145956b8f9a4dda
Кирилл Романенко <romanenko.kirill1995@gmail.com>
2020-04-01 15:56:21 +0300

    small fix 2

--- convert.js
+++ convert.js
@@ -3,8 +3,8 @@ class ConvertTemp {
         this.value = Number(value);
     }
     convert() {
-        let kelvin = this.value + 273.15;
-        let fahrenheit = (this.value * (9 / 5)) + 32;
+        let kelvin = (this.value + 273.15).toFixed(2);
+        let fahrenheit = ((this.value * (9 / 5)) + 32).toFixed(2);
         console.log('Температура (по Цельсию): ', this.value);
         console.log('Температура (в Кельвинах): ', kelvin);
         console.log('Температура (по Фаренгейту): ', fahrenheit);
@@ -16,11 +16,11 @@ class ConvertLen {
         this.value = Number(value);
     }
     convert() {
-        let meters = this.value / 100;
-        let kilometers = this.value / 100000;
-        let feet = this.value / 30.48;
-        let inches = this.value / 2.54;
-        let yards = this.value / 91.44;
+        let meters = (this.value / 100).toFixed(5);
+        let kilometers = (this.value / 100000).toFixed(5);
+        let feet = (this.value / 30.48).toFixed(5);
+        let inches = (this.value / 2.54).toFixed(5);
+        let yards = (this.value / 91.44).toFixed(5);
         console.log('Длина (в сантиметрах): ', this.value);
         console.log('Длина (в метрах): ', meters);
         console.log('Длина (в километрах): ', kilometers);
